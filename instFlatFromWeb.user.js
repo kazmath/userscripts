92f6a7310418da175d16d6dadca4c36d9dcf91df
Matheus H <66137568+kazmath@users.noreply.github.com>
2025-12-26 20:12:23 -0300

    Fix instFlatFromWeb.user.js

--- instFlatFromWeb.user.js
+++ instFlatFromWeb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Install flatpaks from the website
 // @namespace    https://github.com/kazmath/
-// @version      1.2
+// @version      1.3
 // @description  Adds button to install flatpaks, instead of downloading refs. (Manual setup may be necessary)
 // @author       kazmath
 // @match        *://flathub.org/*
@@ -9,11 +9,11 @@
 // @downloadURL  https://github.com/kazmath/userscripts/raw/main/instFlatFromWeb.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=flathub.org
 // @run-at       document-idle
-// @resource     flatpak_icon https://www.svgrepo.com/download/507477/apps.svg
-// @grant        GM_getResourceURL
 // ==/UserScript==
+"use strict";
+const flatpak_icon_svg =
+    "https://raw.githubusercontent.com/flathub-infra/website/refs/heads/main/frontend/public/img/logo/flathub-logo-mini.svg";
 
-'use strict';
 const w = window;
 if (w.instFlatFromWeb) {
     console.log("[instFlatFromWeb] Already running, exiting...");
@@ -23,24 +23,19 @@ if (w.instFlatFromWeb) {
         onblurTriggered: false,
         intervalID: NaN,
         currentLocationPath: w.location.pathname,
-        firstRun: true
+        firstRun: true,
     };
 
-
-    function createButton() {
-        let linkPath = w.location.pathname
+    function createButton(hidden_panel) {
+        let linkPath = w.location.pathname;
         // let header_install = document.querySelectorAll("main > div > header")[0];
-        let hidden_panel = document.querySelectorAll("div[data-headlessui-state] > button ~ div")[0];
+        // let hidden_panel = document.querySelectorAll("div[data-headlessui-portal] > button ~ div")[0];
         let button_div = document.createElement("div");
         let button_a = document.createElement("a");
         let button_h1 = document.createElement("h1");
         let button_icon = document.createElement("img");
 
-        button_div.classList.add(
-            "items-center",
-            "justify-center",
-            "gap-4"
-        );
+        button_div.classList.add("items-center", "justify-center", "gap-4");
         button_a.classList.add(
             "hover:opacity-75",
             "active:opacity-50",
@@ -67,14 +62,16 @@ if (w.instFlatFromWeb) {
         );
         button_h1.innerHTML = "&nbsp;Click to Install Flatpak";
 
-        button_a.href = "appstream:" + linkPath.substring(linkPath.lastIndexOf("/")+1)
+        button_a.href =
+            "appstream:" + linkPath.substring(linkPath.lastIndexOf("/") + 1);
         button_a.addEventListener("click", launchLink);
         button_a.style.backgroundColor = "purple";
         // button_a.innerHTML = "Install<br/>on Machine";
 
-        (async function(button_icon) {
-            button_icon.src = await GM_getResourceURL("flatpak_icon");
-        })(button_icon)
+        button_icon.src = flatpak_icon_svg;
+        // (async function (button_icon) {
+        //     button_icon.src = await GM_getResourceURL("flatpak_icon");
+        // })(button_icon);
 
         button_icon.style.maxWidth = "none";
         button_icon.style.width = "1.8em";
@@ -88,43 +85,49 @@ if (w.instFlatFromWeb) {
     function launchLink(event) {
         // event.preventDefault(); // Prevent the default link behavior
 
-        let onBlurHandler = function() {
+        let onBlurHandler = function () {
             w.instFlatFromWeb.onblurTriggered = true;
             w.removeEventListener("blur", onBlurHandler); // Unregister the onBlur event
         };
         w.addEventListener("blur", onBlurHandler);
 
-        setTimeout(function() {
+        setTimeout(function () {
             if (!w.instFlatFromWeb.onblurTriggered) {
-                alert("Custom script took too long to open.\n\nIs \"appstream:\" protocol set up correctly? If it worked as expected, ignore this message.");
+                alert(
+                    'Custom script took too long to open.\n\nIs "appstream:" protocol set up correctly? If it worked as expected, ignore this message.'
+                );
             }
             w.removeEventListener("blur", onBlurHandler); // Unregister the onBlur event
         }, 2000);
     }
 
-
     // if (!w.instFlatFromWeb.intervalID.isNaN) clearInterval(w.instFlatFromWeb.intervalID);
 
     function repeatMain() {
-
-        let newPath = w.location.pathname;
-        if (w.instFlatFromWeb.firstRun ||
-            newPath !== w.instFlatFromWeb.currentLocationPath &&
-            newPath.startsWith("/apps") &&
-            newPath.indexOf("/search") === -1
-           ) {
-            if (document.querySelectorAll(".inst-flat-from-web-button").length == 0) {
-                createButton();
-            } else {
-                let linkPath = w.location.pathname;
-                document.querySelectorAll(".inst-flat-from-web-button")[0].href = "appstream:" + linkPath.substring(linkPath.lastIndexOf("/")+1);
+        let hidden_panel = document.querySelectorAll(
+            "div[data-headlessui-portal] > button ~ div"
+        )[0];
+        if (
+            document.querySelectorAll(".inst-flat-from-web-button").length ==
+                0 &&
+            hidden_panel != undefined
+        ) {
+            createButton(hidden_panel);
+        } else {
+            let linkPath = w.location.pathname;
+            const created_button = document.querySelectorAll(
+                ".inst-flat-from-web-button"
+            )[0];
+            if (created_button != undefined) {
+                created_button.href =
+                    "appstream:" +
+                    linkPath.substring(linkPath.lastIndexOf("/") + 1);
             }
         }
         w.instFlatFromWeb.currentLocationPath = w.location.pathname;
     }
 
     w.instFlatFromWeb.intervalID = setInterval(repeatMain, 1000);
-
 
     // if (w.instFlatFromWeb.firstRun || w.location.pathname.startsWith("/apps") && w.location.pathname.indexOf("/search") === -1) {
     //     createButton();
